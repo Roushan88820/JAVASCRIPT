113d26e2de053429b9cccae81464b23d4861812a
Roushan88820 <95723776+Roushan88820@users.noreply.github.com>
2022-01-08 21:38:09 +0530

    Add files via upload

---
+++ first_experiance_with_downloading_data_from_web.js
@@ -0,0 +1,26 @@
+// npm install minimist
+
+// npm install axios
+
+// node first_experiance_with_downloading_data_from_web.js --dest="download.html" --url="https://www.espncricinfo.com/series/icc-cricket-world-cup-2019-1144415/match-results"
+
+let minimist = require("minimist");
+let axios = require("axios");
+let fs = require("fs");
+let args = minimist(process.argv);
+
+// console.log(args.dest);
+// console.log(args.url);
+
+let downloadka_Promise = axios.get(args.url);
+
+downloadka_Promise.then(function (response){
+
+    let html = response.data;
+    fs.writeFileSync(args.dest, html, "utf-8");
+    console.log(html)
+
+}).catch(function (error){
+
+    console.log(error);
+})
